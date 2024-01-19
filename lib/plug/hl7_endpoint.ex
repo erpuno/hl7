125bb9a8505610bbd63af2feb1a806b8365434ab
5HT <maxim@synrc.com>
2024-01-19 11:51:39 +0200

    rest

--- lib/plug/hl7_endpoint.ex
+++ lib/plug/hl7_endpoint.ex
@@ -2,8 +2,25 @@ defmodule HL7.Endpoint do
   use Plug.Router
   plug Plug.Logger
   plug :match
-  plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
   plug :dispatch
-  forward "/v5", to: HL7.Router
+  plug(Plug.Parsers, parsers: [:json], json_decoder: Jason)
+  get  "/"          do HL7.Service.root(conn) end
+  get  "/metadata"  do HL7.Service.metadata(conn) end
+  get  "/_history"  do HL7.Service.history(conn) end
+  get  "/_diff"     do HL7.Service.diff(conn) end
+  get  "/$meta"     do HL7.Service.meta(conn) end
+  get  "/$export"   do HL7.Service.export(conn) end
+  post "/"          do HL7.Service.postRoot(conn) end
+  post "/$diff"     do HL7.Service.postDiff(conn) end
+  post "/$reindex"  do HL7.Service.reindex(conn) end
+  post "/$export"   do HL7.Service.postExport(conn) end
+  post "/$meta"     do HL7.Service.postMeta(conn) end
+  get  "/:type/:id"              do HL7.Service.get3(conn,"",type,id) end
+  put  "/:type/:id"              do HL7.Service.put3(conn,"",type,id) end
+  delete ":type/:id"             do HL7.Service.delete3(conn,"",type,id) end
+  post "/_search"                do HL7.Service.post2(conn,"","_search") end
+  post "/:res/_search"           do HL7.Service.post3(conn,"",res,"_search") end
+  post "/:comp/:id/_search"      do HL7.Service.post4(conn,"",comp,id,"_search") end
+  post "/:comp/:id/:res/_search" do HL7.Service.post5(conn,"",comp,id,res,"_search") end
   match _ do send_resp(conn, 404, "Not found!") end
 end
--- lib/plug/hl7_service.ex
+++ lib/plug/hl7_service.ex
@@ -2,20 +2,22 @@ defmodule HL7.Service do
    # Reference Implementation: http://hapi.fhir.org/
    import Plug.Conn
 
-   def postRoot(conn), do: send_resp(conn, 200, [%{}])
-   def postDiff(conn), do: send_resp(conn, 200, [%{}])
-   def postExport(conn), do: send_resp(conn, 200, [%{}])
-   def postMeta(conn), do: send_resp(conn, 200, [%{}])
-   def reindex(conn), do: send_resp(conn, 200, [%{}])
+   def postRoot(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def postDiff(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def postExport(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def postMeta(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def reindex(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
 
-   def root(conn), do: send_resp(conn, 200, [%{}])
-   def metadata(conn), do: send_resp(conn, 200, [%{"resourceType" => "CapabilityStatement"}])
-   def history(conn), do: send_resp(conn, 200, [%{}])
-   def diff(conn), do: send_resp(conn, 200, [%{}])
-   def export(conn), do: send_resp(conn, 200, %{"resourceType" => "CapabilityStatement"})
-   def meta(conn), do:
-       send_resp(conn, 200,
-         %{ "resourceType" => "Parameters",
+   def root(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def metadata(conn), do: send_resp(conn, 200, Jason.encode!([%{"resourceType" => "CapabilityStatement"}]))
+   def history(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def diff(conn), do: send_resp(conn, 200, Jason.encode!([%{}]))
+   def export(conn), do: send_resp(conn, 200, Jason.encode!([%{"resourceType" => "CapabilityStatement"}]))
+   def meta(conn) do
+       :io.format 'HL7/$meta', []
+       conn |> Plug.Conn.put_resp_content_type("application/json") |>
+       send_resp(200,
+         Jason.encode!([%{ "resourceType" => "Parameters",
             "parameters" => [
                %{ "name" => "return",
                   "valueMeta" => %{
@@ -29,13 +31,30 @@ defmodule HL7.Service do
                      "tag" => [%{"system" => "https://ehealth.gov.ua/tag/", "code" => "N", "display" => "normal" }]
                    }
                 } ]
-           })
+           }])) end
 
-   def get3(conn,base,type,id) do send_resp(conn, 200, %{"base" => base, "type" => type, "id" => id}) end
-   def post2(conn,base,qualifier) do send_resp(conn, 200, %{"base" => base, "qualifier" => qualifier}) end
-   def post3(conn,base,resource,qualifier) do send_resp(conn, 200, %{"base" => base, "resource" => resource, "qualifier" => qualifier}) end
-   def post4(conn,base,compartment,id,qualifier) do send_resp(conn, 200, %{"base" => base, "compartment" => compartment, "id" => id, "qualifier" => qualifier}) end
-   def post5(conn,base,compartment,id,resource,qualifier) do send_resp(conn, 200, %{"base" => base, "compartment" => compartment, "resource" => resource,  "id" => id, "qualifier" => qualifier}) end
-   def put3(conn,base,type,id) do send_resp(conn, 200, %{"base" => base, "type" => type, "id" => id}) end
-   def delete3(conn,base,type,id) do send_resp(conn, 200, %{"base" => base, "type" => type, "id" => id}) end
+   def get3(conn,base,type,id) do
+       :io.format 'GET/3 #{base}/#{type}/#{id}', []
+       send_resp(conn, 200, Jason.encode!([%{"base" => base, "type" => type, "id" => id}])) end
+   def get2(conn,base,qualifier) do
+       :io.format 'GET/2 #{base}/#{qualifier}', []
+       send_resp(conn, 200, Jason.encode!([%{"base" => base, "qualifier" => qualifier}])) end
+   def post2(conn,base,qualifier) do
+       :io.format 'POST/2 #{base}/#{qualifier}', []
+       send_resp(conn, 200, Jason.encode!(%{"base" => base, "qualifier" => qualifier})) end
+   def post3(conn,base,resource,qualifier) do
+       :io.format 'POST/3 #{base}/#{resource}/#{qualifier}', []
+       send_resp(conn, 200, Jason.encode!(%{"base" => base, "resource" => resource, "qualifier" => qualifier})) end
+   def post4(conn,base,compartment,id,qualifier) do
+       :io.format 'POST/4 #{base}/#{compartment}/#{qualifier}', []
+       send_resp(conn, 200, Jason.encode!(%{"base" => base, "compartment" => compartment, "id" => id, "qualifier" => qualifier})) end
+   def post5(conn,base,compartment,id,resource,qualifier) do
+       :io.format 'POST/5 #{base}/#{compartment}/#{id}#{qualifier}', []
+       send_resp(conn, 200, Jason.encode!(%{"base" => base, "compartment" => compartment, "resource" => resource,  "id" => id, "qualifier" => qualifier})) end
+   def put3(conn,base,type,id) do
+       :io.format 'PUT/3 #{base}/#{type}#{id}', []
+       send_resp(conn, 200, Jason.encode!(%{"base" => base, "type" => type, "id" => id})) end
+   def delete3(conn,base,type,id) do
+       :io.format 'DELETE/3 #{base}/#{type}#{id}', []
+       send_resp(conn, 200, Jason.encode!(%{"base" => base, "type" => type, "id" => id})) end
 end
