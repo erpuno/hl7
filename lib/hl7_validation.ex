faa29f7682de28089294681490bab42021605793
5HT <maxim@synrc.com>
2024-01-25 15:39:22 +0200

    DeviceAssociation

--- lib/hl7_validation.ex
+++ lib/hl7_validation.ex
@@ -6,7 +6,7 @@ defmodule HL7.Validation do
       [ :Identifier, :Quantity, :Reference, :Location,
         :Extension, :Patient, :Specimen, :Observation,
         :List, :Encounter, :Contract, :Device, :Organization,
-        :DeviceDefinition
+        :DeviceDefinition, :DeviceAssociation
       ]
   end
 
