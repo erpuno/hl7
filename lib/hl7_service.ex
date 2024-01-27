d30f2d31d7da80f9d19ddd355c8432deef0918fb
5HT <maxim@synrc.com>
2024-01-27 08:16:22 +0200

    is POST

--- lib/hl7_service.ex
+++ lib/hl7_service.ex
@@ -15,12 +15,12 @@ defmodule HL7.Service do
    def postExport(conn), do: send_resp(conn, 200, encode([%{}]))
    def postMeta(conn), do: send_resp(conn, 200, encode([%{}]))
    def reindex(conn), do: send_resp(conn, 200, encode([%{}]))
-
    def root(conn), do: send_resp(conn, 200, encode([%{}]))
-   def metadata(conn), do: send_resp(conn, 200, encode([%{"resourceType" => "CapabilityStatement"}]))
    def history(conn), do: send_resp(conn, 200, encode([%{}]))
    def diff(conn), do: send_resp(conn, 200, encode([%{}]))
+   def metadata(conn), do: send_resp(conn, 200, encode([%{"resourceType" => "CapabilityStatement"}]))
    def export(conn), do: send_resp(conn, 200, encode([%{"resourceType" => "CapabilityStatement"}]))
+
    def meta(conn) do
        :io.format 'HL7/$meta', []
        conn |> Plug.Conn.put_resp_content_type("application/json") |>
@@ -40,10 +40,11 @@ defmodule HL7.Service do
                                  "code" => "N",
                               "display" => "normal" }]}}]}])) end
 
-   def get4(conn,base,type,id,spec) do
-       :io.format 'GET/4 #{base}/#{type}/#{id}/#{spec}', []
-       send_resp(conn, 200, encode([%{"base" => base, "type" => type, "id" => id, "spec" => spec}])) end
-   def put4(conn,base,type,id,"$validate" = spec) do
+   def get4(conn,_base,type,id,spec) do
+       :io.format 'GET/4:#{type}/#{id}/#{spec}', []
+       send_resp(conn, 200, encode([%{"type" => type, "id" => id, "spec" => spec}])) end
+
+   def post4(conn,"",type,"" = id,"$validate" = spec) do
        {:ok, body, conn} = Plug.Conn.read_body(conn, [])
        schema = HL7.Loader.loadSchema("#{type}") 
        obj = Jason.decode!(body)
@@ -52,27 +53,34 @@ defmodule HL7.Service do
              {:error, %Xema.ValidationError{message: msg, reason: err}} -> {%{message: msg, reason: err},"error"}
        end
        {message,verify} = res
-       :io.format 'PUT/4 #{base}/#{type}#{id}/#{spec}: ~p (~pKiB)', [verify, :erlang.round(:erlang.size(body) / 1024)]
-       send_resp(conn, 200, encode(%{"base" => base, "type" => type, "id" => Map.get(obj, "id", Map.get(obj, "$id", "")), "spec" => spec, "verify" => message})) end
-   def put4(conn,base,type,id,spec) do
-       :io.format 'PUT/4 #{base}/#{type}#{id}/#{spec}', []
-       send_resp(conn, 200, encode(%{"base" => base, "type" => type, "id" => id, "spec" => spec})) end
-   def delete4(conn,base,type,id,spec) do
-       :io.format 'DELETE/3 #{base}/#{type}#{id}/#{spec}', []
-       send_resp(conn, 200, encode(%{"base" => base, "type" => type, "id" => id, "spec" => spec})) end
-   def get2(conn,base,qualifier) do
-       :io.format 'GET/2 #{base}/#{qualifier}', []
-       send_resp(conn, 200, encode([%{"base" => base, "qualifier" => qualifier}])) end
-   def post2(conn,base,qualifier) do
-       :io.format 'POST/2 #{base}/#{qualifier}', []
-       send_resp(conn, 200, encode(%{"base" => base, "qualifier" => qualifier})) end
-   def post3(conn,base,resource,qualifier) do
-       :io.format 'POST/3 #{base}/#{resource}/#{qualifier}', []
-       send_resp(conn, 200, encode(%{"base" => base, "resource" => resource, "qualifier" => qualifier})) end
-   def post4(conn,base,compartment,id,qualifier) do
-       :io.format 'POST/4 #{base}/#{compartment}/#{qualifier}', []
-       send_resp(conn, 200, encode(%{"base" => base, "compartment" => compartment, "id" => id, "qualifier" => qualifier})) end
-   def post5(conn,base,compartment,id,resource,qualifier) do
-       :io.format 'POST/5 #{base}/#{compartment}/#{id}#{qualifier}', []
-       send_resp(conn, 200, encode(%{"base" => base, "compartment" => compartment, "resource" => resource,  "id" => id, "qualifier" => qualifier})) end
+       :io.format 'POST/4:#{type}#{id}/#{spec}: ~p (~pKiB)', [verify, :erlang.round(:erlang.size(body) / 1024)]
+       send_resp(conn, 200, encode(%{"type" => type, "id" => Map.get(obj, "id", Map.get(obj, "$id", "")), "spec" => spec, "verify" => message})) end
+
+   def post4(conn,_base,compartment,id,qualifier) do
+       :io.format 'POST/4:#{compartment}/#{qualifier}', []
+       send_resp(conn, 200, encode(%{"compartment" => compartment, "id" => id, "qualifier" => qualifier})) end
+
+   def put4(conn,_base,type,id,spec) do
+       :io.format 'PUT/4:#{type}#{id}/#{spec}', []
+       send_resp(conn, 200, encode(%{"type" => type, "id" => id, "spec" => spec})) end
+
+   def delete4(conn,_base,type,id,spec) do
+       :io.format 'DELETE/3:#{type}#{id}/#{spec}', []
+       send_resp(conn, 200, encode(%{"type" => type, "id" => id, "spec" => spec})) end
+
+   def get2(conn,_base,qualifier) do
+       :io.format 'GET/2:#{qualifier}', []
+       send_resp(conn, 200, encode([%{"qualifier" => qualifier}])) end
+
+   def post2(conn,_base,qualifier) do
+       :io.format 'POST/2:#{qualifier}', []
+       send_resp(conn, 200, encode(%{"qualifier" => qualifier})) end
+
+   def post3(conn,_base,resource,qualifier) do
+       :io.format 'POST/3:#{resource}/#{qualifier}', []
+       send_resp(conn, 200, encode(%{"resource" => resource, "qualifier" => qualifier})) end
+
+   def post5(conn,_base,compartment,id,resource,qualifier) do
+       :io.format 'POST/5:#{compartment}/#{id}#{qualifier}', []
+       send_resp(conn, 200, encode(%{"compartment" => compartment, "resource" => resource,  "id" => id, "qualifier" => qualifier})) end
 end
